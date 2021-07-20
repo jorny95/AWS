0f8eb9769acfb4986078abaace26371b0478cc28
jihyun <kjwise1128@gmail.com>
2021-07-20 10:19:01 +0900

    first commit

---
+++ back/models/index.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+// const comment = require('./comment')
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+// db.Comment = comment(sequelize,Sequelize)
+db.Comment = require('./comment')(sequelize,Sequelize)
+
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
